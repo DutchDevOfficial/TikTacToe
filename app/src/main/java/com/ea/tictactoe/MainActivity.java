549f28292251fbcb1078a0a460f0a65f6dc65bf7
Wimme <wimmedg@gmail.com>
2021-01-21 21:07:05 +0100

    FIX
    Rotation and colors

--- app/src/main/java/com/ea/tictactoe/MainActivity.java
+++ app/src/main/java/com/ea/tictactoe/MainActivity.java
@@ -1,13 +1,17 @@
 package com.ea.tictactoe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Source: https://www.youtube.com/channel/UC_Fh8kvtkVPkeihBs42jGcA
 
     private Button[][] buttons = new Button[3][3];
 
@@ -18,11 +22,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView textViewPlayer1;
     private TextView textViewPlayer2;
 
+    private int player1Points = 0;
+    private int player2Points = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        if (savedInstanceState == null) {
+//            roundCount = savedInstanceState.getInt("roundCount");
+//            player1Points = savedInstanceState.getInt("player1Points");
+//            player2Points = savedInstanceState.getInt("player2Points");
+//            player1Turn = savedInstanceState.getBoolean("player1Turn");
+//        }
 
         textViewPlayer1 = findViewById(R.id.text_view_p1);
         textViewPlayer2 = findViewById(R.id.text_view_p2);
@@ -42,7 +55,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonReset.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                player1Points = 0;
+                player2Points = 0;
+                UpdatePointsText();
+                resetBoard();
             }
         });
     }
@@ -65,11 +81,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (checkForWin()){
             if (player1Turn){
                 // player1 won
+                player1Wins();
             }else{
                 //player2 won
+                player2Wins();
             }
         }else if(roundCount == 9){
             // draw - all rounds played
+            draw();
         }else{
             // if no one won yet or no draw yet - change players turn
             player1Turn = !player1Turn;
@@ -120,14 +139,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void player1Wins(){
-
+        player1Points++;
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        UpdatePointsText();
+        resetBoard();
     }
 
     private void player2Wins(){
-
+        player2Points++;
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        UpdatePointsText();
+        resetBoard();
     }
 
     private void draw(){
-        
+        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
+        resetBoard();
+    }
+
+    private void UpdatePointsText(){
+        textViewPlayer1.setText("Player 1: " + player1Points);
+        textViewPlayer2.setText("Player 2: " + player2Points);
+    }
+
+    private void resetBoard(){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                buttons[i][j].setText("");
+            }
+        }
+
+        roundCount = 0;
+        player1Turn = true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("player1Points", player1Points);
+        outState.putInt("player2Points", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
+
+        super.onSaveInstanceState(outState);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1Points");
+        player2Points = savedInstanceState.getInt("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
     }
 }
